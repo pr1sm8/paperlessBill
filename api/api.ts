0cbd8bacd3fa379b14709616a65346f2cb607929
pr1sm8 <saitherockstarnikith@gmail.com>
2022-06-04 12:15:02 +0530

    api /bill/:session_id version - object decode - not tested

--- api/api.ts
+++ api/api.ts
@@ -3,13 +3,21 @@ import express, { Response, Request, NextFunction } from "express";
 import { generateQR } from "../scripts/generateQR";
 const axios = require('axios');
 const redis = require('redis');
-var port = 4000;
+
+const port = process.env.SERVER_PORT || 4000;
+const redis_host = process.env.REDIS_HOST || 'localhost';
+const redis_port = process.env.REDIS_PORT || 6379;
+
 
 const app = express();
-const client = redis.createClient();
 
-client.on('error', (err) => {
-  console.log("Error " + err);
+const redisClient  = redis.createClient({
+    host: redis_host,
+    port: redis_port
+});
+
+redisClient.on('error', err => {
+    console.log('Error ' + err);
 });
 
 function requireQueryParams(params: Array<string>) {
@@ -37,7 +45,7 @@ app.get('/GET', (req, res) => {
     const query = req.query.sessionID!.toString();
   
     // Try fetching the result from Redis first in case we have it cached
-    return client.get(`wikipedia:${query}`, (err, result) => {
+    return redisClient.get(`wikipedia:${query}`, (err, result) => {
       // If that key exist in Redis store
       if (result) {
         const resultJSON = JSON.parse(result);
@@ -49,6 +57,7 @@ app.get('/GET', (req, res) => {
     });
   });
 
+
   
 //   app.set('/SET', (req, res) => {
 app.get('/getQR', (req, res) => {
@@ -58,7 +67,23 @@ app.get('/getQR', (req, res) => {
     const requestBodyObject = JSON.parse(decodedRequestBodyString.toString());
     //const url = generateQR(data);
     res.send(requestBodyObject);
+});
 
+
+
+app.get('/bill/:session_id' , (req , res) =>{
+    redisClient.get(req.params.session_id, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            
+            const encodedRequestBody :string = result.toString();
+            const decodedRequestBodyString = Buffer.from(encodedRequestBody, "base64");
+            const requestBodyObject = JSON.parse(decodedRequestBodyString.toString());
+            //const url = generateQR(data);
+            res.send(requestBodyObject);
+        }
+    });
 })
 
 app.listen(port);
